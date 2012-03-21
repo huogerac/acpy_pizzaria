4c9383febfb0acc9a060ad403133e24b714e001b
Roger Camargo <huogerac@gmail.com>
2012-03-20 22:59:16 -0300

    primeira classe do modelo: Cliente

---
+++ pizzaria/pedidos/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+class Cliente(models.Model):
+    nome = models.CharField(max_length=128)
+    fone = models.CharField(max_length=16)
+    email = models.EmailField(blank=True)
+    
+    def __unicode__(self):
+        return self.nome
+        
+from django.contrib import admin
+admin.site.register(Cliente)
