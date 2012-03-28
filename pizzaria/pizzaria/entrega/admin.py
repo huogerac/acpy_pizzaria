5d9e282c66acaf21c4c40c761c93cd7f354bc7d9
Roger Camargo <huogerac@gmail.com>
2012-03-27 22:53:28 -0300

    formatação da exibição dos pedidos

--- pizzaria/pizzaria/entrega/admin.py
+++ pizzaria/pizzaria/entrega/admin.py
@@ -10,11 +10,14 @@ class ClienteAdmin(admin.ModelAdmin):
     list_filter = ('logradouro',)
     
 class PedidoAdmin(admin.ModelAdmin):
-    list_display = ('hora_inclusao', 'cliente', 'pronto')
+    list_display = ('hora_inclusao', 'cliente', 'pronto', 'partiu')
     
     def hora_inclusao(self, obj):
         return obj.inclusao.strftime('%HH:%MM')
     
+    def partiu(self, obj):
+        return bool(obj.pronto and obj.entregador and obj.partida)
+    partiu.boolean = True
     
 admin.site.register(Cliente, ClienteAdmin)
 admin.site.register(Pedido, PedidoAdmin)
