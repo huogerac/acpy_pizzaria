07174a7bbd0047db821cdd20123e98a2b19a7c09
Roger Camargo <huogerac@gmail.com>
2012-04-03 22:10:04 -0300

    um exemplo de view simples

--- pizzaria/pizzaria/entrega/views.py
+++ pizzaria/pizzaria/entrega/views.py
@@ -1,1 +1,13 @@
 # Create your views here.
+
+from django.http import HttpResponse
+import datetime
+
+def hora_atual(request):
+
+    agora = datetime.datetime.now()
+    html = '<html><h1> Hora: {0}</h1></html>'.format(agora)
+    #html = '<html><h1>' + str(agora) + '</h1></html>'
+
+    return HttpResponse(html)
+
