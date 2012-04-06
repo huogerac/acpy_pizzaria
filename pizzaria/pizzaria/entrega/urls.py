0133b5fefaf1a37c94aca4fd1712efe4a642832e
Roger Camargo <huogerac@gmail.com>
2012-04-05 21:48:42 -0300

    listagem de clientes

--- pizzaria/pizzaria/entrega/urls.py
+++ pizzaria/pizzaria/entrega/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.views.generic import ListView
 
 from .views import HoraView, pizzas_pendentes
-from .models import Pizza
+from .models import Pizza, Cliente
 
 urlpatterns = patterns('',
 
@@ -10,5 +10,7 @@ urlpatterns = patterns('',
     url(r'hora$', HoraView.as_view(), name='hora'),
     #url(r'pizzas$', pizzas_pendentes, name='pizzas'),
     url(r'pizzas$', ListView.as_view(model=Pizza, context_object_name='lista')),
-
+    
+    url(r'clientes$', ListView.as_view(model=Cliente, context_object_name='lista')),    
+    
 )
